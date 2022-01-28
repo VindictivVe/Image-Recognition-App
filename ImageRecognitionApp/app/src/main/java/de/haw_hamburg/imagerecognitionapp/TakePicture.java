ed8da2be11a5e4683baec59c45a20318e5bf2b6b
FlorianWeichbrodt <florian.weichbrodt@dpa-info.com>
2022-01-28 21:12:41 +0100

    "Add miscellaneous"

--- ImageRecognitionApp/app/src/main/java/de/haw_hamburg/imagerecognitionapp/TakePicture.java
+++ ImageRecognitionApp/app/src/main/java/de/haw_hamburg/imagerecognitionapp/TakePicture.java
@@ -1,8 +1,6 @@
 package de.haw_hamburg.imagerecognitionapp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -14,16 +12,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.InterpreterApi;
-import org.tensorflow.lite.InterpreterFactory;
 import org.tensorflow.lite.support.common.FileUtil;
 import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
 
-import org.jetbrains.annotations.NotNull;
 import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.TensorFlowLite;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import android.widget.TextView;
@@ -31,14 +25,9 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class TakePicture extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1;
@@ -48,12 +37,12 @@ public class TakePicture extends AppCompatActivity {
     ImageButton back;
     TextView catOrDogTextView;
     Interpreter tflite;
+    ArrayList<Bitmap> photoHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.take_picture_view);
-        Log.i("test123", "TestOnCreate");
         this.imageView = (ImageView) this.findViewById(R.id.camera_view);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.button_click);
         catOrDogTextView = findViewById(R.id.cat_or_dog_text_view);
@@ -74,8 +63,9 @@ public class TakePicture extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mp.start();
-                //TODO if BitMap not Null safe for photoHistory
+                //TODO if BitMap not Null save for photoHistory
                 Intent intent = new Intent(TakePicture.this, MainMenu.class);
+                intent.putExtra("photo", photo);
                 startActivity(intent);
             }
         });
@@ -108,23 +98,26 @@ public class TakePicture extends AppCompatActivity {
             takePicture.setVisibility(View.INVISIBLE);
             catOrDogTextView.setVisibility(View.VISIBLE);
 
-            if(result[0] > 0.9f){
-                catOrDogTextView.setText("Dog");
-            } else if(result[0] < 0.1f) {
+            if(result[0] == 1 && result[1] == 0 && result[2] == 0){
                 catOrDogTextView.setText("Cat");
+            } else if(result[0] == 0 && result[1] == 1 && result[2] == 0){
+                catOrDogTextView.setText("Dog");
+            } else if(result[0] == 0 && result[1] == 0 && result[2] == 1){
+                catOrDogTextView.setText("Spider");
             } else {
-                catOrDogTextView.setText("Can't tell");
+                catOrDogTextView.setText("None");
             }
+
         }
     }
 
     public float[] doPrediction(Bitmap bitmap) {
         //Initialize ImageProcessor to crop and resize the image: resize method https://www.tensorflow.org/api_docs/python/tf/image/resize?hl=en
         ImageProcessor imageProcessor = new ImageProcessor.Builder()
-                .add(new ResizeOp(64, 64, ResizeOp.ResizeMethod.BILINEAR))
+                .add(new ResizeOp(128, 128, ResizeOp.ResizeMethod.BILINEAR))
                 .build();
 
-        //Create wrapper class for image
+        //Create wrapper class for image, datatype float32
         TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
 
         //Takes in bitmap of taken photo and resizes it
@@ -132,14 +125,15 @@ public class TakePicture extends AppCompatActivity {
         tensorImage = imageProcessor.process(tensorImage);
 
         //TensorBuffer for output data
-        TensorBuffer probabilityBuffer = TensorBuffer.createFixedSize(new int[]{1, 1}, DataType.FLOAT32);
+        //fixedSize of [1,1] because output of net is a 2D array
+        TensorBuffer probabilityBuffer = TensorBuffer.createFixedSize(new int[]{1, 3}, DataType.FLOAT32);
 
         //Load model and pass it into interpreter
         try {
-            MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(TakePicture.this, "model.tflite");
+            MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(TakePicture.this, "model128.tflite");
             tflite = new Interpreter(tfliteModel, new Interpreter.Options());
         } catch (IOException e){
-            Log.e("tfliteSupport", "Error reading model", e);
+            Log.e("result", "Error reading model", e);
         }
 
         //Run model
@@ -149,6 +143,7 @@ public class TakePicture extends AppCompatActivity {
 
         Log.i("result", "Success: " + tensorImage.getBuffer() + " " + probabilityBuffer.getFloatArray());
 
+        //Close interpreter to avoid memory leak
         tflite.close();
 
         return probabilityBuffer.getFloatArray();
